030ee95bc8115887c1ddc215b9ce04ead8b6613f
pawkw <pawkw.users.noreply.github.com>
2021-08-17 15:20:25 -0400

    Added accept/read

--- src/server.rs
+++ src/server.rs
@@ -1,23 +1,33 @@
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct Server {
-    addr: String, 
+    address: String,
 }
 
 impl Server {
-    pub fn new(addr: String) -> Self {
-        Self {
-            addr: addr
-        }
+    pub fn new(address: String) -> Self {
+        Self { address }
     }
 
     pub fn run(self) {
-        println!("HTTP Server listening: {}", self.addr);
+        println!("Listening on {}.", self.address);
 
-        let listener = TcpListener::bind(&self.addr).unwrap();
+        let listener = TcpListener::bind(&self.address).unwrap();
 
         loop {
-            
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received: {}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(error) => println!("Read failed: {}", error),
+                    }
+                },
+                Err(error) => println!("Connection failed: {}", error),
+            }
         }
     }
 }
