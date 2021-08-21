66f3a3440b26fc82b464967256a4e4998ae355f4
pawkw <pawkw@users.noreply.github.com>
2021-08-21 13:23:38 -0400

    Implemented read_file. Added some test html.

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,5 +1,6 @@
 use super::server::Handler;
 use super::http::{Request, Response, StatusCode, Method};
+use std::fs;
 
 pub struct WebsiteHandler {
     public_path: String
@@ -9,13 +10,33 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory traversal: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None
+        }
+    }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::OK, Some("<h1>Welcome to root</h1>".to_string())),
+                "/" => Response::new(StatusCode::OK, self.read_file("index.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::OK, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                }
                 _ => Response::new(StatusCode::NotFound, None)
             }
             _ => Response::new(StatusCode::NotFound, None)
